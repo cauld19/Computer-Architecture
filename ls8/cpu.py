22e750802ac177a197e906e66494005683cb8332
cauld19 <50849710+cauld19@users.noreply.github.com>
2020-10-06 19:57:26 -0700

    step 7

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -20,24 +20,36 @@ class CPU:
 
     def load(self):
         """Load a program into memory."""
-
+        
+        
+        
+        if (len(sys.argv)) != 2:
+            print("remember to pass the second file name")
+            print("usage: python3 fileio.py <second_file_name.py>")
+            sys.exit()
+            
+            
         address = 0
-
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        
+        
+        try:
+            with open(sys.argv[1]) as f:
+                for line in f:
+                    # parse the file to isolate the binary opcodes
+                    possible_number = line[:line.find('#')]
+                    if possible_number == '':
+                        continue # skip to next iteration of loop
+                    
+                    instruction = int(possible_number, 2)
+                    
+                    self.ram[address] = instruction
+                    address += 1
+                    
+                    
+                    
+        except FileNotFoundError:
+            print(f'Error from {sys.argv[0]}: {sys.argv[1]} not found')
+            sys.exit()
 
 
     def alu(self, op, reg_a, reg_b):
